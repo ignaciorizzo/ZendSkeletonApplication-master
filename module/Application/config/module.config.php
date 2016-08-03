374fe096c76aee34503399222f2ddd986d538f0c
ignaciorizzo <ignaciorizzo@dtsconsulting.com.ar>
2016-08-03 20:52:44 -0300

    prueba comentario
    prueba comentario

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -2,7 +2,7 @@
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   http://framework.zend.com/license/new-bsd New BSD License ffffff
  */
 
 namespace Application;
